290c29de670472ca280f89474f93818ef75fbfcb
YuTing4906 <89449703+YuTing4906@users.noreply.github.com>
2024-02-21 01:01:44 +0800

    Add files via upload

---
+++ 作業二/9.24 homework-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, result1, result2;
+
+    cin >> num1;
+    cin >> num2;
+
+    result1 = num1/num2;
+    result2 = num1%num2;
+
+    cout << result1 << endl;
+    cout << result2 << endl;
+}
